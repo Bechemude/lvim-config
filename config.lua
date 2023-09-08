7edba7284d30b81dd8d8a5629324ce43c7f07109
Alexander Sirotin <a@AA.local>
2023-09-08 20:01:46 +0400

    config 1.2

--- config.lua
+++ config.lua
@@ -1,4 +1,9 @@
 --[[
+--require("lspconfig").gopls.setup {
+--    on_attach = function(client, bufnr)
+--            navbuddy.attach(client, bufnr)
+--                end
+--                }
 lvim is the global options object
 
 Linters should be
@@ -8,29 +13,38 @@ an executable
 ]]
 -- THESE ARE EXAMPLE CONFIGS FEEL FREE TO CHANGE TO WHATEVER YOU WANT
 
-vim.opt.termguicolors = true
-vim.opt.relativenumber = true
-vim.opt.timeoutlen = 100
+vim.opt.termguicolors                  = true
+vim.opt.relativenumber                 = true
+vim.opt.timeoutlen                     = 100
+-- lvim.builtin.lualine.style = "default"
 
 -- general
-lvim.log.level = "warn"
-lvim.format_on_save.enabled = true
-lvim.colorscheme = "lunar"
+lvim.log.level                         = "warn"
+lvim.format_on_save.enabled            = true
+lvim.colorscheme                       = "kanagawa"
+lvim.builtin.treesitter.rainbow.enable = true
+
 -- to disable icons and use a minimalist setup, uncomment the following
 -- lvim.use_icons = false
 
 -- keymappings [view all the defaults by pressing <leader>Lk]
-lvim.leader                    = "space"
+lvim.leader                            = "space"
 -- add your own keymapping
-lvim.keys.normal_mode["<C-s>"] = ":w<cr>"
-lvim.keys.normal_mode["<Tab>"] = "%"
-lvim.keys.normal_mode["<S-l>"] = ":BufferLineCycleNext<CR>"
-lvim.keys.normal_mode["<S-h>"] = ":BufferLineCyclePrev<CR>"
+lvim.keys.normal_mode["<C-s>"]         = ":w<cr>"
+lvim.keys.normal_mode["<Tab>"]         = "%"
+lvim.keys.visual_mode["<Tab>"]         = "%"
+-- lvim.keys.deleting["<Tab>"]    = "%"
+lvim.keys.normal_mode["<S-l>"]         = ":BufferLineCycleNext<CR>"
+lvim.keys.normal_mode["<S-h>"]         = ":BufferLineCyclePrev<CR>"
 -- unmap a default keymapping
 -- vim.keymap.del("n", "<C-Up>")
 -- override a default keymapping
 -- lvim.keys.normal_mode["<C-q>"] = ":q<cr>" -- or vim.keymap.set("n", "<C-q>", ":q<cr>" )
 
+
+lvim.builtin.telescope.defaults.file_ignore_patterns = { "node%_modules", ".shadow%-cljs", ".clj%-kondo" }
+
+-- lvim.builtin.telescope.
 -- Change Telescope navigation to use j and k for navigation and n and p for history in both input and normal mode.
 -- we use protected-mode (pcall) just in case the plugin wasn't loaded yet.
 -- local _, actions = pcall(require, "telescope.actions")
@@ -54,16 +68,18 @@ lvim.keys.normal_mode["<S-h>"] = ":BufferLineCyclePrev<CR>"
 -- lvim.builtin.theme.options.style = "storm"
 
 -- Use which-key to add extra bindings with the leader-key prefix
--- lvim.builtin.which_key.mappings["P"] = { "<cmd>Telescope projects<CR>", "Projects" }
--- lvim.builtin.which_key.mappings["t"] = {
---   name = "+Trouble",
---   r = { "<cmd>Trouble lsp_references<cr>", "References" },
---   f = { "<cmd>Trouble lsp_definitions<cr>", "Definitions" },
---   d = { "<cmd>Trouble document_diagnostics<cr>", "Diagnostics" },
---   q = { "<cmd>Trouble quickfix<cr>", "QuickFix" },
---   l = { "<cmd>Trouble loclist<cr>", "LocationList" },
---   w = { "<cmd>Trouble workspace_diagnostics<cr>", "Workspace Diagnostics" },
--- }
+lvim.builtin.which_key.mappings["S"] = { "<cmd>SymbolsOutline<CR>", "Symbols" }
+lvim.builtin.which_key.mappings[";"] = { "<cmd>Telescope projects<CR>", "REPL" }
+lvim.builtin.which_key.mappings["P"] = { "<cmd>Telescope projects<CR>", "Projects" }
+lvim.builtin.which_key.mappings["t"] = {
+  name = "+Trouble",
+  r = { "<cmd>Trouble lsp_references<cr>", "References" },
+  f = { "<cmd>Trouble lsp_definitions<cr>", "Definitions" },
+  d = { "<cmd>Trouble document_diagnostics<cr>", "Diagnostics" },
+  q = { "<cmd>Trouble quickfix<cr>", "QuickFix" },
+  l = { "<cmd>Trouble loclist<cr>", "LocationList" },
+  w = { "<cmd>Trouble workspace_diagnostics<cr>", "Workspace Diagnostics" },
+}
 
 -- TODO: User Config for predefined plugins
 -- After changing plugin config exit and reopen LunarVim, Run :PackerInstall :PackerCompile
@@ -81,6 +97,8 @@ lvim.builtin.treesitter.ensure_installed = {
   "javascript",
   "json",
   "lua",
+  "go",
+  "gomod",
   "python",
   "typescript",
   "tsx",
@@ -92,6 +110,99 @@ lvim.builtin.treesitter.ensure_installed = {
 
 lvim.builtin.treesitter.ignore_install = { "haskell" }
 lvim.builtin.treesitter.highlight.enable = true
+
+-- local navbuddy = require("nvim-navbuddy")
+-- local actions = require("nvim-navbuddy.actions")
+
+-- local navbuddy = pcall(require, "nvim-navbuddy")
+
+------------------------
+-- Dap
+------------------------
+local dap_ok, dapgo = pcall(require, "dap-go")
+if not dap_ok then
+  return
+end
+
+dapgo.setup()
+
+------------------------
+-- LSP GO
+------------------------
+vim.list_extend(lvim.lsp.automatic_configuration.skipped_servers, { "gopls" })
+
+local lsp_manager = require "lvim.lsp.manager"
+lsp_manager.setup("golangci_lint_ls", {
+  on_init = require("lvim.lsp").common_on_init,
+  capabilities = require("lvim.lsp").common_capabilities(),
+  -- require("lspconfig").gopls.setup {
+  --   on_attach = function(client, bufnr)
+  --     navbuddy.attach(client, bufnr)
+  --   end
+  -- }
+})
+
+lvim.builtin.which_key.mappings[";"] = {
+  name = "+GO",
+  -- r = { "<cmd>Trouble lsp_references<cr>", "References" },
+  -- f = { "<cmd>Trouble lsp_definitions<cr>", "Definitions" },
+  -- d = { "<cmd>Trouble document_diagnostics<cr>", "Diagnostics" },
+  -- q = { "<cmd>Trouble quickfix<cr>", "QuickFix" },
+  -- l = { "<cmd>Trouble loclist<cr>", "LocationList" },
+  -- w = { "<cmd>Trouble workspace_diagnostics<cr>", "Workspace Diagnostics" },
+}
+lsp_manager.setup("gopls", {
+  on_attach = function(client, bufnr)
+    require("lvim.lsp").common_on_attach(client, bufnr)
+    local _, _ = pcall(vim.lsp.codelens.refresh)
+    local map = function(mode, lhs, rhs, desc)
+      if desc then
+        desc = desc
+      end
+
+      vim.keymap.set(mode, lhs, rhs, { silent = true, desc = desc, buffer = bufnr, noremap = true })
+    end
+    map("n", "<leader>;i", "<cmd>GoInstallDeps<Cr>", "Install Go Dependencies")
+    map("n", "<leader>;t", "<cmd>GoMod tidy<cr>", "Tidy")
+    map("n", "<leader>;a", "<cmd>GoTestAdd<Cr>", "Add Test")
+    map("n", "<leader>;A", "<cmd>GoTestsAll<Cr>", "Add All Tests")
+    map("n", "<leader>;e", "<cmd>GoTestsExp<Cr>", "Add Exported Tests")
+    map("n", "<leader>;g", "<cmd>GoGenerate<Cr>", "Go Generate")
+    map("n", "<leader>;f", "<cmd>GoGenerate %<Cr>", "Go Generate File")
+    map("n", "<leader>;c", "<cmd>GoCmt<Cr>", "Generate Comment")
+    map("n", "<leader>DT", "<cmd>lua require('dap-go').debug_test()<cr>", "Debug Test")
+  end,
+  on_init = require("lvim.lsp").common_on_init,
+  capabilities = require("lvim.lsp").common_capabilities(),
+  settings = {
+    gopls = {
+      usePlaceholders = true,
+      gofumpt = true,
+      codelenses = {
+        generate = false,
+        gc_details = true,
+        test = true,
+        tidy = true,
+      },
+    },
+  },
+})
+
+local status_ok, gopher = pcall(require, "gopher")
+if not status_ok then
+  return
+end
+
+gopher.setup {
+  commands = {
+    go = "go",
+    gomodifytags = "gomodifytags",
+    gotests = "gotests",
+    impl = "impl",
+    iferr = "iferr",
+  },
+}
+
 
 -- generic LSP settings
 
@@ -116,7 +227,45 @@ lvim.builtin.treesitter.highlight.enable = true
 -- ---see the full default list `:lua print(vim.inspect(lvim.lsp.automatic_configuration.skipped_servers))`
 -- vim.list_extend(lvim.lsp.automatic_configuration.skipped_servers, { "pyright" })
 -- local opts = {} -- check the lspconfig documentation for a list of all possible options
--- require("lvim.lsp.manager").setup("pyright", opts)
+-- require("lvim.lsp.manager").setup("tsserver", {
+--   handlers = {
+--     ["textDocument/publishDiagnostics"] = function(
+--       _,
+--       result,
+--       ctx,
+--       config
+--     )
+--       if result.diagnostics == nil then
+--         return
+--       end
+
+--       -- ignore some tsserver diagnostics
+--       local idx = 1
+--       while idx <= #result.diagnostics do
+--         local entry = result.diagnostics[idx]
+
+--         local formatter = require('format-ts-errors')[entry.code]
+--         entry.message = formatter and formatter(entry.message) or entry.message
+
+--         -- codes: https://github.com/microsoft/TypeScript/blob/main/src/compiler/diagnosticMessages.json
+--         if entry.code == 80001 then
+--           -- { message = "File is a CommonJS module; it may be converted to an ES module.", }
+--           table.remove(result.diagnostics, idx)
+--         else
+--           idx = idx + 1
+--         end
+--       end
+
+--       vim.lsp.diagnostic.on_publish_diagnostics(
+--         _,
+--         result,
+--         ctx,
+--         config
+--       )
+--     end,
+--   },
+-- })
+
 
 -- ---remove a server from the skipped list, e.g. eslint, or emmet_ls. !!Requires `:LvimCacheReset` to take effect!!
 -- ---`:LvimInfo` lists which server(s) are skipped for the current filetype
@@ -125,7 +274,7 @@ lvim.builtin.treesitter.highlight.enable = true
 -- end, lvim.lsp.automatic_configuration.skipped_servers)
 
 -- -- you can set a custom on_attach function that will be used for all the language servers
--- -- See <https://github.com/neovim/nvim-lspconfig#keybindings-and-completion>
+-- -- See <https://github.com/ua require("nvim-navbuddy").open()ovim/nvim-lspconfig#keybindings-and-completion>
 -- lvim.lsp.on_attach_callback = function(client, bufnr)
 --   local function buf_set_option(...)
 --     vim.api.nvim_buf_set_option(bufnr, ...)
@@ -133,49 +282,194 @@ lvim.builtin.treesitter.highlight.enable = true
 --   --Enable completion triggered by <c-x><c-o>
 --   buf_set_option("omnifunc", "v:lua.vim.lsp.omnifunc")
 -- end
+--
+
+-- lvim.lsp.on_attach_callback = function(client, bufnr)
+--   navbuddy.attach(client, bufnr)
+-- end
+
+
+
+-- local null_ls_status_ok, null_ls = pcall(require, "null-ls")
+-- if not null_ls_status_ok then
+-- 	return
+-- end
+
 
 -- -- set a formatter, this will override the language server formatting capabilities (if it exists)
--- local formatters = require "lvim.lsp.null-ls.formatters"
--- formatters.setup {
---   { command = "black", filetypes = { "python" } },
---   { command = "isort", filetypes = { "python" } },
---   {
---     -- each formatter accepts a list of options identical to https://github.com/jose-elias-alvarez/null-ls.nvim/blob/main/doc/BUILTINS.md#Configuration
---     command = "prettier",
---     ---@usage arguments to pass to the formatter
---     -- these cannot contain whitespaces, options such as `--line-width 80` become either `{'--line-width', '80'}` or `{'--line-width=80'}`
---     extra_args = { "--print-with", "100" },
---     ---@usage specify which filetypes to enable. By default a providers will attach to all the filetypes it supports.
---     filetypes = { "typescript", "typescriptreact" },
---   },
--- }
+local formatters = require "lvim.lsp.null-ls.formatters"
+formatters.setup {
+  { command = "goimports", filetypes = { "go" } },
+  { command = "gofumpt",   filetypes = { "go" } },
+  {
+    -- each formatter accepts a list of options identical to https://github.com/jose-elias-alvarez/null-ls.nvim/blob/main/doc/BUILTINS.md#Configuration
+    command = "prettier",
+    ---@usage arguments to pass to the formatter
+    -- these cannot contain whitespaces, options such as `--line-width 80` become either `{'--line-width', '80'}` or `{'--line-width=80'}`
+    -- extra_args = { "--print-with", "100" },
+    ---@usage specify which filetypes to enable. By default a providers will attach to all the filetypes it supports.
+    filetypes = { "typescript", "typescriptreact", "javascript", "javascriptreact", "vue", "svelte" },
+  },
+}
 
 -- -- set additional linters
--- local linters = require "lvim.lsp.null-ls.linters"
--- linters.setup {
---   { command = "flake8", filetypes = { "python" } },
---   {
---     -- each linter accepts a list of options identical to https://github.com/jose-elias-alvarez/null-ls.nvim/blob/main/doc/BUILTINS.md#Configuration
---     command = "shellcheck",
---     ---@usage arguments to pass to the formatter
---     -- these cannot contain whitespaces, options such as `--line-width 80` become either `{'--line-width', '80'}` or `{'--line-width=80'}`
---     extra_args = { "--severity", "warning" },
---   },
---   {
---     command = "codespell",
---     ---@usage specify which filetypes to enable. By default a providers will attach to all the filetypes it supports.
---     filetypes = { "javascript", "python" },
---   },
--- }
+local linters = require "lvim.lsp.null-ls.linters"
+linters.setup {
+  {
+    command = "eslint",
+    ---@usage specify which filetypes to enable. By default a providers will attach to all the filetypes it supports.
+    filetypes = { "css", "typescript", "typescriptreact", "javascript", "javascriptreact", "vue", "svelte" },
+  },
+  -- { command = "flake8", filetypes = { "python" } },
+  -- {
+  --   -- each linter accepts a list of options identical to https://github.com/jose-elias-alvarez/null-ls.nvim/blob/main/doc/BUILTINS.md#Configuration
+  --   command = "shellcheck",
+  --   ---@usage arguments to pass to the formatter
+  --   -- these cannot contain whitespaces, options such as `--line-width 80` become either `{'--line-width', '80'}` or `{'--line-width=80'}`
+  --   extra_args = { "--severity", "warning" },
+  -- },
+  -- {
+  --   command = "codespell",
+  --   ---@usage specify which filetypes to enable. By default a providers will attach to all the filetypes it supports.
+  --   filetypes = { "javascript", "python" },
+  -- },
+}
+
 
 -- Additional Plugins
 lvim.plugins = {
-  { 'Olical/conjure' },
+  {
+    "rebelot/kanagawa.nvim",
+    --     config = function()
+    --       require('kanagawa').setup({
+    -- })
+  },
+  {
+    "Pocco81/true-zen.nvim",
+    config = function()
+      require("true-zen").setup {
+        -- your config goes here
+        -- or just leave it empty :)
+      }
+    end,
+  },
+  {
+    'michaelb/sniprun',
+    run = 'sh ./install.sh',
+    config = function()
+      require 'sniprun'.setup({
+        interpreter_options = {
+          Go_original = {
+            compiler = "gccgo"
+          }
+        }
+      })
+    end
+  },
+  {
+    "rmagatti/goto-preview",
+    config = function()
+      require('goto-preview').setup {
+        width = 120,              -- Width of the floating window
+        height = 25,              -- Height of the floating window
+        default_mappings = false, -- Bind default mappings
+        debug = false,            -- Print debug information
+        opacity = nil,            -- 0-100 opacity level of the floating window where 100 is fully transparent.
+        post_open_hook = nil      -- A function taking two arguments, a buffer and a window to be ran as a hook.
+        -- You can use "default_mappings = true" setup option
+        -- Or explicitly set keybindings
+        -- vim.cmd("nnoremap gpd <cmd>lua require('goto-preview').goto_preview_definition()<CR>")
+        -- vim.cmd("nnoremap gpi <cmd>lua require('goto-preview').goto_preview_implementation()<CR>")
+        -- vim.cmd("nnoremap gP <cmd>lua require('goto-preview').close_all_win()<CR>")
+      }
+    end
+  },
+  {
+    "ray-x/lsp_signature.nvim",
+    event = "BufRead",
+    config = function() require "lsp_signature".on_attach() end,
+  },
+  {
+    "simrat39/symbols-outline.nvim",
+    config = function()
+      require('symbols-outline').setup()
+    end
+  },
+  {
+    'anuvyklack/fold-preview.nvim',
+    requires = 'anuvyklack/keymap-amend.nvim',
+    config = function()
+      require('fold-preview').setup({
+        auto = 400
+        -- Your configuration goes here.
+      })
+    end
+  },
+  {
+    'anuvyklack/pretty-fold.nvim',
+    config = function()
+      require('pretty-fold').setup()
+    end
+  },
+  'smolck/command-completion.nvim',
+  {
+    "sindrets/diffview.nvim",
+    event = "BufRead",
+  },
+
+  {
+    'ray-x/navigator.lua',
+    requires = {
+      { 'ray-x/guihua.lua',     run = 'cd lua/fzy && make' },
+      { 'neovim/nvim-lspconfig' },
+    },
+  },
+  {
+    "Fildo7525/pretty_hover",
+    config = function()
+      require("pretty_hover").setup(options)
+    end
+  },
+  "davidosomething/format-ts-errors.nvim",
+  {
+    "windwp/nvim-spectre",
+    event = "BufRead",
+    config = function()
+      require("spectre").setup()
+    end,
+  },
+  -- { 'rose-pine/neovim', as = 'rose-pine' },
+  'mg979/vim-visual-multi',
+  "olexsmir/gopher.nvim",
+  "leoluz/nvim-dap-go",
+  -- { 'ray-x/go.nvim' },
+
+  { 'vimwiki/vimwiki' },
+  -- { 'Olical/conjure' },
+
   {
     "folke/trouble.nvim",
     cmd = "TroubleToggle",
   },
+
   { "gpanders/editorconfig.nvim" },
+
+  {
+    "ggandor/lightspeed.nvim",
+    event = "BufRead",
+  },
+
+  {
+    "JoosepAlviste/nvim-ts-context-commentstring",
+    event = "BufRead",
+    -- config = {
+    --   clojure = ";; %s",
+    --   clj = ";; %s"
+    -- }
+  },
+  {
+    "p00f/nvim-ts-rainbow",
+  },
   {
     "itchyny/vim-cursorword",
     event = { "BufEnter", "BufNewFile" },
@@ -189,20 +483,58 @@ lvim.plugins = {
       vim.api.nvim_command("augroup END")
     end
   },
+
+  {
+    "tpope/vim-surround",
+
+    -- make sure to change the value of `timeoutlen` if it's not triggering correctly, see https://github.com/tpope/vim-surround/issues/117
+    -- setup = function()
+    --  vim.o.timeoutlen = 500
+    -- end
+  },
+
+  {
+    "kevinhwang91/nvim-bqf",
+    event = { "BufRead", "BufNew" },
+    config = function()
+      require("bqf").setup({
+        auto_enable = true,
+        preview = {
+          win_height = 12,
+          win_vheight = 12,
+          delay_syntax = 80,
+          border_chars = { "┃", "┃", "━", "━", "┏", "┓", "┗", "┛", "█" },
+        },
+        func_map = {
+          vsplit = "",
+          ptogglemode = "z,",
+          stoggleup = "",
+        },
+        filter = {
+          fzf = {
+            action_for = { ["ctrl-s"] = "split" },
+            extra_opts = { "--bind", "ctrl-o:toggle-all", "--prompt", "> " },
+          },
+        },
+      })
+    end,
+  },
+
   {
     "windwp/nvim-ts-autotag",
     config = function()
       require("nvim-ts-autotag").setup()
     end,
   },
+
   {
     "romgrk/nvim-treesitter-context",
     config = function()
       require("treesitter-context").setup {
-        enable = true, -- Enable this plugin (Can be enabled/disabled later via commands)
+        enable = true,   -- Enable this plugin (Can be enabled/disabled later via commands)
         throttle = true, -- Throttles plugin updates (may improve performance)
-        max_lines = 0, -- How many lines the window should span. Values <= 0 mean no limit.
-        patterns = { -- Match patterns for TS nodes. These get wrapped to match at word boundaries.
+        max_lines = 0,   -- How many lines the window should span. Values <= 0 mean no limit.
+        patterns = {     -- Match patterns for TS nodes. These get wrapped to match at word boundaries.
           -- For all filetypes
           -- Note that setting an entry here replaces all other patterns for this entry.
           -- By setting the 'default' entry below, you can control which nodes you want to
